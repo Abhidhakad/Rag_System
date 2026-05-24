c6e5b2b1dac28c8942241be33ca9b44ff4b8f186
Abhishek <nabhishek734@gmail.com>
2026-05-24 17:11:36 +0530

    connect frontend to render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://rag-system-duei.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 60000,
 })
